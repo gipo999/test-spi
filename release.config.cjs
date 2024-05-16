18efab286d92603610e1ef66548cc6e724d29d57
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-16 15:28:01 +0200

    fix: add comments to releaser action

--- release.config.cjs
+++ release.config.cjs
@@ -2,8 +2,8 @@ const BREAKING_KEYWORDS = ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"];
 module.exports = {
   // choose the branch to release from
   // release: {
-  //   branches: [
   //   // Default: ['+([0-9])?(.{+([0-9]),x}).x', 'master', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}]
+  //   branches: [
   //     "main",
   //     "dev",
   //     "next",
