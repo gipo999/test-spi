33db460342092cd788a5e3fe8b9e671a99763922
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-20 16:22:22 +0200

    refactor: add missing javadoc comments, format code for checkstyle

--- src/main/java/net/syn3rgy/core/spi/ImplementationSelector.java
+++ src/main/java/net/syn3rgy/core/spi/ImplementationSelector.java
@@ -3,9 +3,21 @@ package net.syn3rgy.core.spi;
 import java.util.List;
 import java.util.Optional;
 
+/** Interface for selecting an implementation of a service. */
 @FunctionalInterface
 public interface ImplementationSelector {
 
+  /**
+   * Chooses an implementation of a service.
+   *
+   * @param clazz the class of the service
+   * @param implementations the implementations of the service
+   * @param params the parameters for choosing the implementation
+   * @return the chosen implementation
+   * @param <T> the type of the service
+   */
   <T extends NamedService> Optional<T> choose(
+
+      /** The class of the service */
       Class<T> clazz, List<T> implementations, Optional<ImplementationParams> params);
 } // ServiceProviderChooser
--- src/main/java/net/syn3rgy/core/spi/NamedService.java
+++ src/main/java/net/syn3rgy/core/spi/NamedService.java
@@ -1,10 +1,19 @@
 package net.syn3rgy.core.spi;
 
+/** Interface for services that have a priority. */
 @FunctionalInterface
 public interface NamedService {
 
+  /** Returns the name of the service implementation. */
   String IMPL_STANDARD_NAME = "standard";
+
+  /** Returns the name of the custom service implementation. */
   String IMPL_CUSTOM_NAME = "custom";
 
+  /**
+   * Returns the name of the service implementation.
+   *
+   * @return the name of the service implementation
+   */
   String getServiceImplementationName();
 }
