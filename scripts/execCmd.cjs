0223825ef98b66e324a96f21eaa3ad9b40fa2625
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-23 12:11:52 +0200

    fix: fix gradle cmds not working in windows

--- scripts/execCmd.cjs
+++ scripts/execCmd.cjs
@@ -1,13 +1,8 @@
-const { spawnSync } = require("child_process");
+const { execSync } = require("child_process");
 
 module.exports = {
   execCmd: (cmd) => {
     console.log(`Running: ${cmd}`);
-
-    const [exec, ...args] = cmd.split(" ");
-
-    const currentCmd = spawnSync(exec, args, {
-      stdio: "inherit",
-    });
+    execSync(cmd, { stdio: "inherit" });
   },
 };
